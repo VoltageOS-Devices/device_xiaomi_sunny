29052a06c213b847c538515d79998ab432f2ebd1
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    sunny: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -42,10 +42,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
